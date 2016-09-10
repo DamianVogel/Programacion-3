5b1460a396694f617aff6cf2abcb2cdce36fe6fe
DamianVogel <rodolfovogel@gmail.com>
2016-09-10 15:06:00 -0300

    Despues de perder lo hecho en clase, arranco de nuevo con el ejercicio 25a

---
+++ Clase03/Aplicacion25a.php
@@ -0,0 +1,27 @@
+
+
+
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="animacion.css">
+
+
+	<title></title>
+</head>
+<body>
+		<div class="CajaInicio animated bouncheInRight "> 
+
+		<form method="post" id="SuperficieDelRectangulo">
+			<input type="text" 	name="base"		placerholder="Ingrese la base">
+			<input type="text"	name="altura"	placerholder="Ingrese la altura">
+
+			<input type="submit" class="MiBotonUTNJuego">
+		</form>
+
+
+
+</div>
+</body>
+</html>
