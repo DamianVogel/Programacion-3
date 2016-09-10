faae91b0e9d4f69e6e54f6684115afb777d877c2
DamianVogel <rodolfovogel@gmail.com>
2016-09-10 19:38:20 -0300

    Funciona pero no como yo quiero
    Funciona, inclusive con el boton "volver" pero aun no puedo lograr que
    la primera vez no muestre los input de base y altura

--- Clase03/Aplicacion25a.php
+++ Clase03/Aplicacion25a.php
@@ -1,23 +1,51 @@
 
 
-
+<?php  ?>
 
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 	<link rel="stylesheet" type="text/css" href="animacion.css">
 
+	<script>
+		function Volver() {
+	    
+	   window.history.back()		
+		
+		}
+	
+
+
+	</script>
 
 	<title></title>
 </head>
 <body>
 		<div class="CajaInicio animated bouncheInRight "> 
 
-		<form method="post" id="SuperficieDelRectangulo">
-			<input type="text" 	name="base"		placerholder="Ingrese la base">
-			<input type="text"	name="altura"	placerholder="Ingrese la altura">
 
-			<input type="submit" class="MiBotonUTNJuego">
+
+		<form method="post" name="form1" id="SuperficieDelRectangulo">
+						<?php  if(!isset($_POST['resultado'])){?> 
+						
+							<input type="text" 		name="base"					placeholder="Ingrese la base"> 										
+							<input type="text"		name="altura"				placeholder="Ingrese la altura"> 
+							<input type="submit" 	class="MiBotonUTNJuego">
+							<input type="reset" 	class="MiBotonUTNJuego">
+					
+						<?php } ?>
+
+				<?php if(isset($_POST['base']) && isset($_POST['altura'])) {  ?>
+							
+				<input type="text"	name="resultado" value="<?php echo $_POST['base']*$_POST['altura']; ?>" readonly> 			
+				
+				<button onclick="Volver()" class="MiBotonUTNJuego"> Volver </button>
+
+
+
+				<?php  } ?>
+
+			
 		</form>
 
 
@@ -25,3 +53,4 @@
 </div>
 </body>
 </html>
+
