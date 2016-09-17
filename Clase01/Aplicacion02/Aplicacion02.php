ec19e5cc799b5b407a6ad274373e081d351cefa7
DamianVogel <rodolfovogel@gmail.com>
2016-09-17 17:37:28 -0300

    Logre entender como funciona el Date y el GetDate
    Ya estoy insertando el codigo para el "case 1" del switch

--- Clase01/Aplicacion02/Aplicacion02.php
+++ Clase01/Aplicacion02/Aplicacion02.php
@@ -19,26 +19,30 @@
                                         <?php 
 if(isset($_POST['fname']))
   {
+    $dia = getdate(date('U'));
+    echo var_dump($dia);
   switch ($_POST['fname']) {
-                            case '1'    :?>
-                                                    <Br> <input type="text" name="muestra1" value="Entro en el case 1" readonly>  
-                                        <?php
-                            break;                          
-                            case'2':
-                                        ?>
-                                                    <Br> <input type="text" name="muestra2" value="Entro en el case 2" readonly> 
-                                                
-                                        <?php  
-                            break;
-                            case '3':
-                                        ?>
-                                                    <Br> <input type="text" name="muestra3" value="Entro en el case 3" readonly>
-                                        <?php
-                            default:
-                            break;
-                           }  
+                              case '1':    
+                                          ?>
+                                                      <Br> <input type="text" name="muestra1" value="<?php echo "Hoy es ".$dia['mday']." / ".$dia['mon']." / ".$dia['year']."y son las "; ?>" readonly>  
+                                          <?php
+                                              
+                              break;                          
+                              case'2':
+                                          ?>
+                                                      <Br> <input type="text" name="muestra2" value="Entro en el case 2" readonly> 
+                                                  
+                                          <?php  
+                              break;
+                              case '3':
+                                          ?>
+                                                      <Br> <input type="text" name="muestra3" value="Entro en el case 3" readonly>
+                                          <?php
+                              default:
+                              break;
+                             }  
 
-  
+    
 
   }                         
                                         ?>
