54cef8dbb10e4501edd12700f0ddb623c3fe89c4
DamianVogel <rodolfovogel@gmail.com>
2016-09-17 16:19:57 -0300

    Ya casi esta terminado el Switch incrustando PHP en el form

--- Clase01/Aplicacion02/Aplicacion02.php
+++ Clase01/Aplicacion02/Aplicacion02.php
@@ -4,21 +4,46 @@
 <body>
 
 
-<form method="post" name="fecha">
+                                                    <form method="post" name="fecha">
+                                                        
+                                                        Elija como desea mostrar la fecha:<Br> 
+                                                        1-<Br> 
+                                                        2-<Br>
+                                                        3-<Br>
+                                                        <input type="text" name="fname">
+                                                        <input type="submit">
+
+
+
+
+                                        <?php 
+if(isset($_POST['fname']))
+  {
+  switch ($_POST['fname']) {
+                            case '1'    :?>
+                                                    <Br> <input type="text" name="muestra1" value="Entro en el case 1" readonly>  
+                                        <?php
+                            break;                          
+                            case'2':
+                                        ?>
+                                                    <Br> <input type="text" name="muestra2" value="Entro en el case 2" readonly> 
+                                                
+                                        <?php  
+                            break;
+                            case '3':
+                                        ?>
+                                                    <Br> <input type="text" name="muestra3" value="Entro en el case 3" readonly>
+                                        <?php
+                            default:
+                            break;
+                           }  
+
   
-  Elija como desea mostrar la fecha:<Br> 
-  1-<Br>
-  2-<Br>
-  3-<Br>
-  <input type="text" name="fname">
-  <input type="submit">
 
-
-  <input type="text" name="muestra" value="<?php echo $eleccion ?>">
-
-
-
-</form>
+  }                         
+                                        ?>
+                                          
+                                                    </form>
 
 
 
