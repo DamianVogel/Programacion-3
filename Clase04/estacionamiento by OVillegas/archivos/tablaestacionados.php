8cdc7b75a66f01a256d9d7f31db1e7418b69c1ed
DamianVogel <rodolfovogel@gmail.com>
2016-09-18 20:13:07 -0300

    Agrego el proyecto de estacionamiento del Profesor

---
+++ Clase04/estacionamiento by OVillegas/archivos/tablaestacionados.php
@@ -0,0 +1,2 @@
+ <table border=1><th> patente </th><th> Ingreso</th><tr> <td> lxu031 </td> <td> 2016-09-19 00:12:39
+</td> </tr> </table>
---
+++ Clase04/estacionamiento by OVillegas/js/funcionAutoCompletar.js
@@ -0,0 +1,21 @@
+$(function(){
+			  var patentes = [ 
+
+			   {value: "lxu031" , data: " 2016-09-19 00:12:39 " }, 
+
+			   
+			  ];
+			  
+			  // setup autocomplete function pulling from patentes[] array
+			  $('#autocomplete').autocomplete({
+			    lookup: patentes,
+			    onSelect: function (suggestion) {
+			      var thehtml = '<strong>patente: </strong> ' + suggestion.value + ' <br> <strong>ingreso: </strong> ' + suggestion.data;
+			      $('#outputcontent').html(thehtml);
+			         $('#botonIngreso').css('display','none');
+      						console.log('aca llego');
+			    }
+			  });
+			  
+
+			});
